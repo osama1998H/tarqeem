39e87a0e89843ded8dbf91aee93d51f93af91e25
osama1998H <osamamuhammed555@gmail.com>
2026-01-05 20:45:15 +0300

    fix: collapse nested if for clippy lint

--- src/codegen/llvm/codegen.rs
+++ src/codegen/llvm/codegen.rs
@@ -325,17 +325,17 @@ impl LlvmCodegen {
                             field,
                             value,
                         } => {
-                            if anon_objects.contains(&object.0) && field.class.0 == "__anonymous__"
+                            if anon_objects.contains(&object.0)
+                                && field.class.0 == "__anonymous__"
+                                && !seen_fields.contains(&field.name)
                             {
-                                if !seen_fields.contains(&field.name) {
-                                    // Get the type of the value being set
-                                    let field_ty = var_types
-                                        .get(&value.0)
-                                        .cloned()
-                                        .unwrap_or(IrType::Ptr(Box::new(IrType::Void)));
-                                    fields.push((field.name.clone(), field_ty));
-                                    seen_fields.insert(field.name.clone());
-                                }
+                                // Get the type of the value being set
+                                let field_ty = var_types
+                                    .get(&value.0)
+                                    .cloned()
+                                    .unwrap_or(IrType::Ptr(Box::new(IrType::Void)));
+                                fields.push((field.name.clone(), field_ty));
+                                seen_fields.insert(field.name.clone());
                             }
                         }
                         _ => {}
